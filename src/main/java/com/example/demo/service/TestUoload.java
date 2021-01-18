a98858bff69ecf19b21ccd7ea0706bb5cdc8321c
huangyongqi <z805744426@qq.com>
2021-01-18 17:01:57 +0800

    lal

---
+++ src/main/java/com/example/demo/service/TestUoload.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.example.demo.service;
+
+/**
+ *
+ * @author 黄永琦
+ * @since 2021-01-18
+ * @see  
+ */
+public class TestUoload {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+    }
+
+}
